9717f776bd21621113257dbf6075bc1216aa178e
mays <mays@caiban.net>
2015-09-09 21:39:54 +0800

    Add some feature

--- pc-erp/src/main/java/net/caiban/pc/erp/controller/ProductController.java
+++ pc-erp/src/main/java/net/caiban/pc/erp/controller/ProductController.java
@@ -123,7 +123,9 @@ public class ProductController extends BaseController {
 	@RequestMapping
 	public ModelAndView picture(HttpServletRequest request, ModelMap model, 
 			Integer id){
-		
+		if(id==null||id.intValue()<=0){
+			return new ModelAndView("redirect:index.do");
+		}
 		model.put("id", id);
 		return null;
 	}
@@ -131,7 +133,21 @@ public class ProductController extends BaseController {
 	@RequestMapping
 	public ModelAndView price(HttpServletRequest request, ModelMap model,
 			Integer id){
+		if(id==null||id.intValue()<=0){
+			return new ModelAndView("redirect:index.do");
+		}
 		model.put("id", id);
 		return null;
 	}
+	
+	@RequestMapping
+	public ModelAndView group(HttpServletRequest request, ModelMap model,
+			Integer id){
+		if(id==null||id.intValue()<=0){
+			return new ModelAndView("redirect:index.do");
+		}
+		model.put("id", id);
+		return null;
+	}
+	
 }
