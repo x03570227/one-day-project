800e8943ea8513955db56cae449bf8650d8ada30
mays <mays@caiban.net>
2016-04-24 11:03:01 +0800

    Add weui validate

--- pc-erp/src/main/webapp/js/everyday/weui.validate.js
+++ pc-erp/src/main/webapp/js/everyday/weui.validate.js
@@ -10,14 +10,35 @@ define(		["jquery", "template"],
                 var rule = JSON.parse(ruleStr);
                 var value = jQuery(this).val();
 
-                if(typeof rule["require"] != "undefined" && rule["require"]){
+                if(typeof rule["required"] != "undefined" && rule["required"]){
                     if(value==""){
                         def.addCellWarn(jQuery(this).parent().parent());
-                        def.addCellFt(jQuery(this).parent().parent());
+                        //def.addCellFt(jQuery(this).parent().parent());
+                        def.disableSubmit(formid);
+                        return ;
                     }
                 }
-//            weui_btn_disabled
+
+                if(typeof rule["pattern"] != "undefined" && rule["pattern"]!=""){
+                    if(value!=""){
+                        var pattern =eval(rule["pattern"]);
+                        if(!pattern.test(value)){
+                            def.addCellWarn(jQuery(this).parent().parent());
+                            //def.addCellFt(jQuery(this).parent().parent());
+                            def.disableSubmit(formid);
+                            return ;
+                        }
+                    }
+                }
+
+                def.removeCellWarn(jQuery(this).parent().parent());
+                //def.removeCellFt(jQuery(this).parent().parent());
             });
+
+            if(jQuery("#"+formid+" input[data-validate]").length>0){
+                def.disableSubmit(formid);
+            }
+
         }
 
         def["addCellWarn"]=function(obj){
@@ -27,19 +48,41 @@ define(		["jquery", "template"],
             obj.addClass("weui_cell_warn");
         }
 
-        def["addCellFt"]=function(obj){
-            var children = obj.children(".weui_cell_ft");
+        //def["addCellFt"]=function(obj){
+        //    var children = obj.children(".weui_cell_ft");
+        //
+        //    if(children.length>0){
+        //        return ;
+        //    }
+        //
+        //    var source ="<div class=\"weui_cell_ft\">"+
+        //        "<i class=\"weui_icon_warn\"></i>"+
+        //        "</div>";
+        //    var render = template.compile(source);
+        //
+        //    obj.append(render);
+        //}
 
-            if(children.length>0){
-                return ;
-            }
+        def["removeCellWarn"]=function(obj){
+            obj.removeClass("weui_cell_warn");
+        }
 
-            var source ="<div class=\"weui_cell_ft\">"+
-                "<i class=\"weui_icon_warn\"></i>"+
-                "</div>";
-            var render = template.compile(source);
+        //def["removeCellFt"]=function(obj){
+        //    //var children = obj.children(".weui_cell_ft");
+        //
+        //    //if(children.length>0){
+        //    //
+        //    //}
+        //    obj.remove(".weui_cell_ft");
+        //}
 
-            obj.append(render);
+        def["disableSubmit"]=function(formid){
+            jQuery("#"+formid+" input[type=submit]").each(function(){
+                if(jQuery(this).hasClass("weui_btn_disabled")){
+                    return ;
+                }
+                jQuery(this).addClass("weui_btn_disabled");
+            });
         }
 
 		return def;
